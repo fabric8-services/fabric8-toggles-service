72fc78388029a8849826f3052411c8841f67bc97
Xavier Coulon <xcoulon@redhat.com>
2017-11-29 11:46:50 +0100

    Refactor code to allow for toogle client replacement in features controller
    Include test where the mock unleash client can be injected in the toogle service.
    This mock client has a similar behavior compared to the real client, i.e.,
    it verifies the features with the configured strategies.
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ featuretoggles/toggles_client_wrapper.go
@@ -0,0 +1,57 @@
+package featuretoggles
+
+import (
+	"fmt"
+
+	"github.com/Unleash/unleash-client-go"
+	unleashcontext "github.com/Unleash/unleash-client-go/context"
+)
+
+type UnleashClient interface {
+	Ready() <-chan bool
+	GetEnabledFeatures(ctx *unleashcontext.Context) []string
+	Close() error
+}
+
+// Client the toggle client
+type Client struct {
+	UnleashClient UnleashClient
+}
+
+// ToggleServiceConfiguration the configuration to the Toggle service
+type ToggleServiceConfiguration interface {
+	// GetToggleServiceAppName() string
+	GetTogglesURL() string
+}
+
+// NewClient returns a new client to the toggle feature service
+func NewClient(config ToggleServiceConfiguration) (*Client, error) {
+	a := config.GetTogglesURL()
+	fmt.Println(a)
+	client, err := unleash.NewClient(
+		unleash.WithAppName("fabric8-ui"),
+		unleash.WithUrl(config.GetTogglesURL()),
+		unleash.WithStrategies(&EnableByGroupIDStrategy{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+	// wait until client did perform a data sync
+	<-client.Ready()
+	return &Client{UnleashClient: client}, nil
+
+}
+
+// Close closes the underlying Unleash client
+func (c *Client) Close() error {
+	return c.UnleashClient.Close()
+}
+
+// GetEnabledFeatures returns the names of enabled features for the given user
+func (c *Client) GetEnabledFeatures(groupID string) []string {
+	return c.UnleashClient.GetEnabledFeatures(&unleashcontext.Context{
+		Properties: map[string]string{
+			GroupID: groupID,
+		},
+	})
+}
