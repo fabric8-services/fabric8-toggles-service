92ff7615bc31eddd4fc10cb7807e8615fab97442
Jaime Melis <jmelis@redhat.com>
2018-04-11 23:43:42 +0200

    Build on RHEL

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -16,7 +16,7 @@ function setup_build_environment() {
     [ -f inherit-env ] && . inherit-env
 
     # We need to disable selinux for now, XXX
-    /usr/sbin/setenforce 0
+    /usr/sbin/setenforce 0 || :
 
     yum -y install docker make golang git
     service docker start
@@ -35,7 +35,7 @@ function setup_golang() {
   # Show Go version
   go version
   # Setup GOPATH
-  mkdir $HOME/go $HOME/go/src $HOME/go/bin $HOME/go/pkg
+  mkdir -p  $HOME/go $HOME/go/src $HOME/go/bin $HOME/go/pkg
   export GOPATH=$HOME/go
   export PATH=$GOPATH/bin:$PATH
 }
@@ -59,11 +59,12 @@ setup_workspace
 
 cd $GOPATH/src/github.com/fabric8-services/fabric8-toggles-service
 echo "HEAD of repository `git rev-parse --short HEAD`"
+make login REGISTRY_USER=${DEVSHIFT_USERNAME} REGISTRY_PASSWORD=${DEVSHIFT_PASSWORD}
 make all
 
 bash <(curl -s https://codecov.io/bash) -f coverage.txt -t cbdff99f-9158-4128-8dec-ef6afb6d78ab
 
 if [[ "$JOB_NAME" = "devtools-fabric8-toggles-service-build-master" ]]; then
     TAG=$(echo ${GIT_COMMIT} | cut -c1-${DEVSHIFT_TAG_LEN})
-    make push-openshift REGISTRY_USER=${DEVSHIFT_USERNAME} REGISTRY_PASSWORD=${DEVSHIFT_PASSWORD} IMAGE_TAG=${TAG}
+    make push-openshift IMAGE_TAG=${TAG}
 fi
